79fe2516fa519d473fb5340a5e277641cbf3c86f
allogn <no-email@email.com>
2020-02-22 18:10:42 +0100

    Cleaning and Readme update

---
+++ Readme.md
@@ -0,0 +1,3 @@
+This repository is supplementary to the publication
+
+A. Logins, P. Karras, "On the Robustness of Cascade Diffusion under Node Attacks", In *Proceedings of the Web Conference*, 2020.
--- setup.py
+++
@@ -1,8 +0,0 @@
-import sys
-from setuptools import setup
-if sys.version_info < (3,7):
-    sys.exit('Sorry, Python < 3.7 is not supported') # required to have sorted dict
-
-setup(name='src',
-      version='0.0.1'
-)
